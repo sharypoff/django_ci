aad83a980e02e0ff8404b80a6a1b67420ccfa817
Aleksei Sharypov <a.sharypoff@gmail.com>
2022-05-04 16:57:28 +0300

    fix tabs

--- demopages/tests.py
+++ demopages/tests.py
@@ -2,10 +2,10 @@ from django.test import Client, TestCase
 
 
 class IndexTestCase(TestCase):
-	def setUp(self) -> None:
-		self.client = Client()
+    def setUp(self) -> None:
+        self.client = Client()
 
-	def test_get_index_endpoint(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.content, b'Hello, world')
+    def test_get_index_endpoint(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello, world')
--- demopages/views.py
+++ demopages/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(response):
-	return HttpResponse("Hello, world")
+    return HttpResponse("Hello, world")
